5ebb222f702bd8206e94673b85cf2c753e29ba00
ishikorohon <69950510+ishikorohon@users.noreply.github.com>
2020-10-05 11:28:41 +0900

    カテゴリー作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
+
+  has_one_attached :image
+  belongs_to :user
+
+  validates :name, :text, :category, :status, :delivery, :prefecture_id, :shipping_id, :price, :image, presence:true
+  validates :category, :status, :delivery, :prefecture_id, :shipping_id, numericality: { other_than: 1 } 
+  validates :price,numericality:{only_integer:true,greater_than_or_equal_to:300,less_than_or_equal_to:9999999}
 end
